03061e56f388beba7221fe1f09ae8a6b11724598
DiegoJohnsonL <54253758+DiegoJohnsonL@users.noreply.github.com>
2022-06-30 08:30:35 -0500

    Update CreateJobTest.java

--- src/test/java/com/example/jobagapi/funcional/CreateJobTest.java
+++ src/test/java/com/example/jobagapi/funcional/CreateJobTest.java
@@ -17,7 +17,7 @@ public class CreateJobTest {
 
   @Before
   public void setUp() {
-    System.setProperty("webdriver.chrome.driver", "src/test/java/com/example/jobagapi/funcional/drivers/chromedriver-v101.exe");
+    System.setProperty("webdriver.chrome.driver", "src/test/java/com/example/jobagapi/funcional/drivers/chromedriver-v103.exe");
     driver = new ChromeDriver();
     wait = new WebDriverWait(driver, Duration.ofSeconds(5).getSeconds());
   }
